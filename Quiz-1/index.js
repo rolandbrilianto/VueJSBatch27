67072eefb17323db44a0699d676c30ac22e222fb
Roland Brilianto <rolandbrilianto@gmail.com>
2021-08-21 13:51:28 +0700

    Function Penghitung Jumlah Kata

---
+++ Quiz-1/index.js
@@ -0,0 +1,9 @@
+//No 2
+function jumlah_kata(kalimat) {
+  return kalimat.split(" ").length;
+}
+
+var kalimat_1 = "Halo nama saya Roland Brilianto";
+var kalimat_2 = "Saya Luffy";
+console.log(jumlah_kata(kalimat_1));
+console.log(jumlah_kata(kalimat_2));
